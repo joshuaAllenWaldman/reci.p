02dc8f8fb71896e363a094f74621b13d56bbc0c0
joshuaWaldman <joshuaallenwaldman@gmail.com>
2021-01-15 14:03:49 -0800

    Models and Views files added but not filled out. connection to mongo made

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/project-one';
+
+mongoose.connect (connectionString, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected on ${connectionString}`);
+})
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose disconnected`);
+})
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose Error: ${err}`);
+})
+
--- server.js
+++ server.js
@@ -8,15 +8,20 @@ PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
+
+
+//Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
+
+//HOME
 app.get('/', (req, res) => {
-  res.send('CONNECTED')
+  res.render('index')
 });
 
 
-
+//Port Listener
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
