5058fc0fd4b0852f79dfcd2639a7a3c0470e03d6
luvycunanan <luvycunanan@gmail.com>
2021-01-15 15:19:54 -0800

    Signup page done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+router.get('/signup', (req, res) => {
+  res.render('signup')
+});
+
 module.exports = router;
