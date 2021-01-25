a60aa3aacbe9b4f74a630e75b8971d7c75615cc7
joshuaWaldman <joshuaallenwaldman@gmail.com>
2021-01-25 10:32:26 -0800

    secret fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(`${__dirname}/public`))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use(session({
-  secret: 'milo the barking dog',
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
